373f0d16909b3b67c5c2c8359279f58c32703071
felipe <jonathanfelipe0111@hotmail.com>
2024-03-13 18:11:49 -0500

    feat: Se Añade animacion SignUp
    Implementa de animacion de entradas de titulos, subtitulos y formulario

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -2,6 +2,7 @@
 
 import { Button, Checkbox, Input } from "@nextui-org/react";
 import Link from "next/link";
+import 'animate.css';
 
 export const SignUpForm = () => {
     return (
@@ -10,15 +11,15 @@ export const SignUpForm = () => {
             <div className="lg:flex w-1/2 hidden bg-gray-500 relative items-center bg-[url('/img/hero-skate.jpg')]">
                 <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
                 <div className="w-full px-24 z-10">
-                    <h1 className="text-5xl font-bold text-left tracking-wide">¡Regístrate y comparte tus mejores trucos!</h1>
-                    <p className="text-3xl my-4"> ¡Es hora de mostrar tu talento en lo que amas!</p>
+                    <h1 className="text-5xl font-bold text-left tracking-wide animate__animated animate__backInLeft">¡Regístrate y comparte tus mejores trucos!</h1>
+                    <p className="text-3xl my-4 animate__animated animate__backInUp"> ¡Es hora de mostrar tu talento en lo que amas!</p>
                 </div>
             </div>
             <div className="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0 " >
                 <div className="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center bg-[url('/img/hero-skate.jpg')]">
                     <div className="absolute bg-black opacity-90 inset-0 z-0  bg-gradient-to-r from-slate-900 to-slate-900"></div>
                 </div>
-                <div className="w-full py-6 z-20 flex flex-col justify-items-center">
+                <div className="w-full py-6 z-20 flex flex-col justify-items-center animate__animated animate__backInRight">
                     <div className="w-full px-24 z-10 lg:hidden">
                         <p className="text-3xl my-4"> ¡Es hora de mostrar tu talento!</p>
                     </div>
